542c40535e21613180358dca6b8120d72d820149
Camilo Arango <lomica95@hotmail.com>
2018-06-13 15:11:11 -0500

    Fin de Streams

--- src/test/java/co/com/s4n/training/java/jdk/StreamsSuite.java
+++ src/test/java/co/com/s4n/training/java/jdk/StreamsSuite.java
@@ -49,7 +49,8 @@ public class StreamsSuite {
 
     @Test
     public void testStreamsVacios(){
-        Optional<String> first = Arrays.asList()
+        List<String> list = new ArrayList<>();
+        Optional<String> first = list
                 .stream()
                 .findFirst();
 
@@ -75,6 +76,17 @@ public class StreamsSuite {
 
     }
 
+    @Test
+    public void testStreamsCollection(){
+        IntStream stream = IntStream.range(1, 4);
+        List<Integer> list = stream.boxed().collect(Collectors.toList());
+
+        assertEquals(3, list.size());
+        assertTrue(list.contains(1));
+        assertTrue(list.contains(2));
+        assertTrue(list.contains(3));
+
+    }
 
     @Test
     public void testStreams6(){
@@ -83,6 +95,23 @@ public class StreamsSuite {
                 .average();
 
         assertEquals(5D,average.orElseGet(()->666),0D);
+
+    }
+
+    @Test
+    public void testStreamsSinStreams() {
+
+        List<String> numeros =Arrays.asList("a5", "a6", "a7");
+        int max=0, aux;
+
+        for (String numero: numeros) {
+            aux = Integer.valueOf(numero.substring(1));
+            if(aux > max){
+                max = aux;
+            }
+        }
+
+        assertEquals(7, max);
 
     }
 
@@ -202,11 +231,14 @@ public class StreamsSuite {
     @Test
     public void testStream11(){
         // Cuántos elementos pasan por el stream?
+        System.out.println("Cuantos elementos pasan por el stream?");
         boolean b = Stream.of("d2", "a2", "b1", "b3", "c")
                 .map(s -> {
+                    System.out.println(s);
                     return s.toUpperCase();
                 })
                 .anyMatch(s -> {
+                    System.out.println(s);
                     return s.startsWith("A");
                 });
 
@@ -232,6 +264,12 @@ public class StreamsSuite {
     @Test
     public void testStreams13() {
         //TODO: cambia el orden de map y filter
+        List<String> collect = Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    return s.startsWith("A");
+                }).map(s -> {
+                    return s.toUpperCase();
+                }).collect(Collectors.toList());
         assertTrue(true);
     }
 
@@ -258,8 +296,8 @@ public class StreamsSuite {
         boolean b = streamSupplier.get().anyMatch(s -> true);
         boolean b1 = streamSupplier.get().noneMatch(s -> true);
 
-        assert(b);
-        assert(b1);
+        assertTrue(b);
+        assertFalse(b1);
 
     }
 
@@ -311,7 +349,6 @@ public class StreamsSuite {
                         .collect(Collectors.toList());
 
         assertTrue(filtered.size()==2);
-
     }
 
     @Test
